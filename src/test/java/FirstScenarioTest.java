920f6c6e87adc69f027a2de1fee84c7cc658cd39
PlzSayTy <58561190+PlzSayTy@users.noreply.github.com>
2020-03-12 18:58:45 +0300

    Update FirstScenarioTest.java

--- src/test/java/FirstScenarioTest.java
+++ src/test/java/FirstScenarioTest.java
@@ -19,7 +19,6 @@ public class FirstScenarioTest {
         System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
         driver = new ChromeDriver();
         baseUrl = "https://www.rgs.ru/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
     @Test
@@ -27,40 +26,42 @@ public class FirstScenarioTest {
         Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
         driver.get(baseUrl);
         driver.findElement(By.xpath("//ol[contains(@class, 'nav navbar-nav navbar-nav-rgs-menu pull-left')]")).click();
-        driver.findElement(By.xpath("//*[contains(text(),'ДМС')]")).click();
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement((By.xpath("//h1[contains(text(), 'ДМС')]")))));
-        Assert.assertEquals("ДМС — добровольное медицинское страхование", driver.findElement((By.xpath("//h1[contains(text(), 'ДМС')]"))).getText());
+        driver.findElement(By.xpath("//*[contains(text(),'Р”РњРЎ')]")).click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement((By.xpath("//h1[contains(text(), 'Р”РњРЎ')]")))));
+        Assert.assertEquals("Р”РњРЎ вЂ” РґРѕР±СЂРѕРІРѕР»СЊРЅРѕРµ РјРµРґРёС†РёРЅСЃРєРѕРµ СЃС‚СЂР°С…РѕРІР°РЅРёРµ", driver.findElement((By.xpath("//h1[contains(text(), 'Р”РњРЎ')]"))).getText());
         driver.findElement(By.xpath("//*[contains(@class, 'btn btn-default text-uppercase hidden-xs adv-analytics-navigation-desktop-floating-menu-button')]")).click();
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//b[contains(text(), 'Заявка на добровольное медицинское страхование')]"))));
-        Assert.assertEquals("Заявка на добровольное медицинское страхование", driver.findElement((By.xpath("//b[contains(text(), 'Заявка на добровольное медицинское страхование')]"))).getText());
-        driver.findElement(By.xpath("//input[contains(@name, 'LastName')]")).sendKeys("Трубецкой");
-        driver.findElement(By.xpath("//input[contains(@name, 'FirstName')]")).sendKeys("Владимир");
-        driver.findElement(By.xpath("//input[contains(@name, 'MiddleName')]")).sendKeys("Андреевич");
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//b[contains(text(), 'Р—Р°СЏРІРєР° РЅР° РґРѕР±СЂРѕРІРѕР»СЊРЅРѕРµ РјРµРґРёС†РёРЅСЃРєРѕРµ СЃС‚СЂР°С…РѕРІР°РЅРёРµ')]"))));
+        Assert.assertEquals("Р—Р°СЏРІРєР° РЅР° РґРѕР±СЂРѕРІРѕР»СЊРЅРѕРµ РјРµРґРёС†РёРЅСЃРєРѕРµ СЃС‚СЂР°С…РѕРІР°РЅРёРµ", driver.findElement((By.xpath("//b[contains(text(), 'Р—Р°СЏРІРєР° РЅР° РґРѕР±СЂРѕРІРѕР»СЊРЅРѕРµ РјРµРґРёС†РёРЅСЃРєРѕРµ СЃС‚СЂР°С…РѕРІР°РЅРёРµ')]"))).getText());
+        driver.findElement(By.xpath("//input[contains(@name, 'LastName')]")).sendKeys("РўСЂСѓР±РµС†РєРѕР№");
+        driver.findElement(By.xpath("//input[contains(@name, 'FirstName')]")).sendKeys("Р’Р»Р°РґРёРјРёСЂ");
+        driver.findElement(By.xpath("//input[contains(@name, 'MiddleName')]")).sendKeys("РђРЅРґСЂРµРµРІРёС‡");
         driver.findElement(By.xpath("//select[contains(@name, 'Region')]")).click();
-        driver.findElement(By.xpath("//option[contains(text(), 'Москва')]")).click();
-        driver.findElement(By.xpath("/html/body/div[7]/div/div/div/div[2]/div[2]/form/div[2]/div[5]/input")).click();
-        driver.findElement(By.xpath("/html/body/div[7]/div/div/div/div[2]/div[2]/form/div[2]/div[5]/input")).sendKeys("9168378267");
+        driver.findElement(By.xpath("//option[contains(text(), 'РњРѕСЃРєРІР°')]")).click();
+        driver.findElement(By.xpath("//*[contains(text(),'РўРµР»РµС„РѕРЅ')]/../input")).click();
+        driver.findElement(By.xpath("//*[contains(text(),'РўРµР»РµС„РѕРЅ')]/../input")).sendKeys("9168378267");
         driver.findElement(By.xpath("//input[contains(@name, 'Email')]")).sendKeys("qwertyqwerty");
         driver.findElement(By.xpath("//input[contains(@name, 'ContactDate')]")).click();
         driver.findElement(By.xpath("//input[contains(@name, 'ContactDate')]")).sendKeys("10.03.2020");
+        driver.findElement(By.xpath("//input[contains(@name, 'ContactDate')]")).sendKeys("\n");
         driver.findElement(By.xpath("//textarea[contains(@name, 'Comment')]")).click();
-        driver.findElement(By.xpath("//textarea[contains(@name, 'Comment')]")).sendKeys("Оставляю комментарий");
+        driver.findElement(By.xpath("//textarea[contains(@name, 'Comment')]")).sendKeys("РћСЃС‚Р°РІР»СЏСЋ РєРѕРјРјРµРЅС‚Р°СЂРёР№");
         driver.findElement(By.xpath("//input[contains(@class, 'checkbox')]")).click();
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(text(), 'Введите адрес электронной почты')]"))));
-        System.out.println(driver.findElement(By.xpath("//input[contains(@name, 'LastName')]")).getAttribute("value"));
-        Assert.assertEquals("Трубецкой", driver.findElement(By.xpath("//input[contains(@name, 'LastName')]")).getAttribute("value"));
-        Assert.assertEquals("Владимир", driver.findElement(By.xpath("//input[contains(@name, 'FirstName')]")).getAttribute("value"));
-        Assert.assertEquals("Андреевич", driver.findElement(By.xpath("//input[contains(@name, 'MiddleName')]")).getAttribute("value"));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(text(), 'Р’РІРµРґРёС‚Рµ Р°РґСЂРµСЃ СЌР»РµРєС‚СЂРѕРЅРЅРѕР№ РїРѕС‡С‚С‹')]"))));
+        Assertions.assertAll("РЈРїСЃ, С‡С‚Рѕ-С‚Рѕ Р·Р°РїРѕР»РЅРёР»РѕСЃСЊ РЅРµ С‚Р°Рє", (Executable) () ->{
+        Assert.assertEquals("РўСЂСѓР±РµС†РєРѕР№", driver.findElement(By.xpath("//input[contains(@name, 'LastName')]")).getAttribute("value"));
+        Assert.assertEquals("Р’Р»Р°РґРёРјРёСЂ", driver.findElement(By.xpath("//input[contains(@name, 'FirstName')]")).getAttribute("value"));
+        Assert.assertEquals("РђРЅРґСЂРµРµРІРёС‡", driver.findElement(By.xpath("//input[contains(@name, 'MiddleName')]")).getAttribute("value"));
         Assert.assertEquals("77", driver.findElement(By.xpath("//select[contains(@name, 'Region')]")).getAttribute("value"));
         Assert.assertEquals("qwertyqwerty", driver.findElement(By.xpath("//input[contains(@name, 'Email')]")).getAttribute("value"));
         Assert.assertEquals("+7 (916) 837-82-67", driver.findElement(By.xpath("/html/body/div[7]/div/div/div/div[2]/div[2]/form/div[2]/div[5]/input")).getAttribute("value"));
-        Assert.assertEquals("10.03.2020", driver.findElement(By.xpath("//input[contains(@name, 'ContactDate')]")).getAttribute("value"));
-        Assert.assertEquals("Оставляю комментарий", driver.findElement(By.xpath("//textarea[contains(@name, 'Comment')]")).getAttribute("value"));
+        Assert.assertEquals("12.03.2020", driver.findElement(By.xpath("//input[contains(@name, 'ContactDate')]")).getAttribute("value"));
+        Assert.assertEquals("РћСЃС‚Р°РІР»СЏСЋ РєРѕРјРјРµРЅС‚Р°СЂРёР№", driver.findElement(By.xpath("//textarea[contains(@name, 'Comment')]")).getAttribute("value"));
         driver.findElement(By.xpath("//button[contains(@id, 'button-m')]")).click();
-        Assert.assertEquals("Введите адрес электронной почты", driver.findElement(By.xpath("//span[contains(text(), 'Введите адрес электронной почты')]")).getText());
+        Assert.assertEquals("Р’РІРµРґРёС‚Рµ Р°РґСЂРµСЃ СЌР»РµРєС‚СЂРѕРЅРЅРѕР№ РїРѕС‡С‚С‹", driver.findElement(By.xpath("//span[contains(text(), 'Р’РІРµРґРёС‚Рµ Р°РґСЂРµСЃ СЌР»РµРєС‚СЂРѕРЅРЅРѕР№ РїРѕС‡С‚С‹')]")).getText());
+        });
     }
     @After
     public void closeTest(){
-        driver.close();
+        driver.quit();
     }
 }
